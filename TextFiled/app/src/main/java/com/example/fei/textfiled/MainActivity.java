233a2e604e5b4048154b668017fd31732115beb2
Ann <lifeiwanguk@gmail.com>
2018-06-14 10:33:55 -0700

    layout

--- TextFiled/app/src/main/java/com/example/fei/textfiled/MainActivity.java
+++ TextFiled/app/src/main/java/com/example/fei/textfiled/MainActivity.java
@@ -2,8 +2,18 @@ package com.example.fei.textfiled;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public void clickFunction(View view){
+//        EditText myTextField = (EditText)findViewById(R.id.myTextFiled);
+//
+//        Toast.makeText(MainActivity.this,"Hi ther " + myTextField.getText().toString() , Toast.LENGTH_LONG).show();
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
